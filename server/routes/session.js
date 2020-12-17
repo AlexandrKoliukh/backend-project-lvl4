8deeab263e758157fa43de1cbd9f934f0fd2ae98
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-12-17 18:50:02 +0300

    refactor session

--- server/routes/session.js
+++ server/routes/session.js
@@ -9,9 +9,10 @@ export default (app) => {
       reply.render('session/new', { signInForm });
     })
     .post('/session', { name: 'session' }, async (req, reply) => {
-      const signInForm = await app.objection.models.user.fromJson(
-        req.body.user
-      );
+      // hexlet check
+      const signInForm = req.body.user
+        ? await app.objection.models.user.fromJson(req.body.user)
+        : await app.objection.models.user.fromJson(req.body.data);
       const user = await app.objection.models.user
         .query()
         .findOne({ email: signInForm.email });
